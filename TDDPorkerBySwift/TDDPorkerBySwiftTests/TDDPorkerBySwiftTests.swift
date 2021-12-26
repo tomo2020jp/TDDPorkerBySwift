cfcdcb087140bd4b39656a4f8a18a03685bd9f54
bruno1911 <mr.xxxx.bank.band@gmail.com>
2021-12-26 17:37:14 +0900

    refactoring

--- TDDPorkerBySwift/TDDPorkerBySwiftTests/TDDPorkerBySwiftTests.swift
+++ TDDPorkerBySwift/TDDPorkerBySwiftTests/TDDPorkerBySwiftTests.swift
@@ -11,13 +11,15 @@ import XCTest
 class TDDPorkerBySwiftTests: XCTestCase {
 
     func testInitializeCard(){
-        let card = Card(suit: .heart, rank: .three)
+        var card: Card
+        
+        card = Card(suit: .heart, rank: .three)
         XCTAssertEqual(card.suit, .heart)
         XCTAssertEqual(card.rank, .three)
         
-        let card2 = Card(suit: .spade, rank: .jack)
-        XCTAssertEqual(card2.suit, .spade)
-        XCTAssertEqual(card2.rank, .jack)
+        card = Card(suit: .spade, rank: .jack)
+        XCTAssertEqual(card.suit, .spade)
+        XCTAssertEqual(card.rank, .jack)
     }
     
     override func setUpWithError() throws {
