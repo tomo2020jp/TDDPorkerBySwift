3783af45b64fd72180d2ead892a8ef53d96dccf6
bruno1911 <mr.xxxx.bank.band@gmail.com>
2021-12-26 17:55:51 +0900

    不要なテストを削除

--- TDDPorkerBySwift/TDDPorkerBySwiftTests/TDDPorkerBySwiftTests.swift
+++ TDDPorkerBySwift/TDDPorkerBySwiftTests/TDDPorkerBySwiftTests.swift
@@ -9,18 +9,6 @@ import XCTest
 @testable import TDDPorkerBySwift
 
 class TDDPorkerBySwiftTests: XCTestCase {
-
-    func testInitializeCard(){
-        var card: Card
-        
-        card = Card(rank: .three, suit: .heart)
-        XCTAssertEqual(card.suit, .heart)
-        XCTAssertEqual(card.rank, .three)
-        
-        card = Card(rank: .jack, suit: .spade)
-        XCTAssertEqual(card.suit, .spade)
-        XCTAssertEqual(card.rank, .jack)
-    }
     
     func testCardNotation(){
         var card: Card
