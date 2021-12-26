196dd68d0d5b2d030055dc6d934e44495c3f1fa4
bruno1911 <mr.xxxx.bank.band@gmail.com>
2021-12-26 17:33:40 +0900

    first test

--- TDDPorkerBySwift/TDDPorkerBySwift/Card.swift
+++ TDDPorkerBySwift/TDDPorkerBySwift/Card.swift
@@ -10,10 +10,25 @@ import Foundation
 struct Card{
     enum Suit{
         case heart
+        case spade
+        case club
+        case diamond
     }
     
     enum Rank{
+        case one
+        case two
         case three
+        case four
+        case five
+        case six
+        case seven
+        case eight
+        case nine
+        case ten
+        case jack
+        case queen
+        case king
     }
     
     let suit: Suit
--- TDDPorkerBySwift/TDDPorkerBySwiftTests/TDDPorkerBySwiftTests.swift
+++ TDDPorkerBySwift/TDDPorkerBySwiftTests/TDDPorkerBySwiftTests.swift
@@ -14,6 +14,10 @@ class TDDPorkerBySwiftTests: XCTestCase {
         let card = Card(suit: .heart, rank: .three)
         XCTAssertEqual(card.suit, .heart)
         XCTAssertEqual(card.rank, .three)
+        
+        let card2 = Card(suit: .spade, rank: .jack)
+        XCTAssertEqual(card2.suit, .spade)
+        XCTAssertEqual(card2.rank, .jack)
     }
     
     override func setUpWithError() throws {
