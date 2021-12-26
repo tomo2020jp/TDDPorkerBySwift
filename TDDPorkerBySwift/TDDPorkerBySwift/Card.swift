0f41e6817d1f5fbc42d5dc463f238668f295a030
bruno1911 <mr.xxxx.bank.band@gmail.com>
2021-12-26 17:54:45 +0900

    明白な実装

--- TDDPorkerBySwift/TDDPorkerBySwift/Card.swift
+++ TDDPorkerBySwift/TDDPorkerBySwift/Card.swift
@@ -31,8 +31,8 @@ struct Card{
         case king = "K"
     }
     
-    let suit: Suit
     let rank: Rank
+    let suit: Suit
     
     var notation: String{
         return  rank.rawValue + suit.rawValue
--- TDDPorkerBySwift/TDDPorkerBySwiftTests/TDDPorkerBySwiftTests.swift
+++ TDDPorkerBySwift/TDDPorkerBySwiftTests/TDDPorkerBySwiftTests.swift
@@ -13,21 +13,21 @@ class TDDPorkerBySwiftTests: XCTestCase {
     func testInitializeCard(){
         var card: Card
         
-        card = Card(suit: .heart, rank: .three)
+        card = Card(rank: .three, suit: .heart)
         XCTAssertEqual(card.suit, .heart)
         XCTAssertEqual(card.rank, .three)
         
-        card = Card(suit: .spade, rank: .jack)
+        card = Card(rank: .jack, suit: .spade)
         XCTAssertEqual(card.suit, .spade)
         XCTAssertEqual(card.rank, .jack)
     }
     
     func testCardNotation(){
         var card: Card
-        card = Card(suit: .heart, rank: .three)
+        card = Card(rank: .three, suit: .heart)
         XCTAssertEqual(card.notation , "3♥")
         
-        card = Card(suit: .spade, rank: .jack)
+        card = Card(rank: .jack, suit: .spade)
         XCTAssertEqual(card.notation, "J♠︎")
     }
 
